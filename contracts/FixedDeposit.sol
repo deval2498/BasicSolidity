b1585559a42cb64db63d19d6869f1af8ed290ee5
deval2498 <deval.minocha24@gmail.com>
2021-12-16 13:19:11 +0530

    Hmm

--- contracts/FixedDeposit.sol
+++ contracts/FixedDeposit.sol
@@ -4,23 +4,43 @@ pragma solidity ^0.8.4;
 import './ERC20.sol';
 
 contract FixedDeposit {
+    uint256 public ethBal;
     mapping(address => uint256) public balance;
     event Received(address _from, uint256 _amount);
     event Transferred(address _to, uint256 _amount);
+    uint256 TimePeriod;
+    uint256 deadline;
+
+    constructor() {
+        TimePeriod = 3 seconds;
+    }
 
 
-    receive() external payable {
-        balance[address(this)] += msg.value;
+    fallback() external payable {
+        ethBal += msg.value;
         emit Received(msg.sender,msg.value);
     }
 
-    function deposit(uint256 _amount) public {
-        require(balance[msg.sender] == 0, 'You have already made a deposit!');
-        balance[msg.sender] += _amount;
+    receive() external payable {
+        ethBal += msg.value;
+        emit Received(msg.sender , msg.value);
     }
 
-    function withdraw(ERC20 token,address _to) public {
-        token.transfer(_to ,balance[_to]);
+    function deposit(uint256 _amount,ERC20 _token) public {
+        require(balance[msg.sender] == 0, 'You have already made a deposit!');
+        require(_token.balanceOf(msg.sender) >= _amount, 'you dont have enough balance!');
+        _token.approve(address(this), _amount);
+        _token.transferFrom(msg.sender,address(this),_amount);
+        deadline = block.timestamp + TimePeriod;
+        emit Transferred(address(this),_amount);
+    }
+
+    function withdraw(ERC20 _token,address payable _to) public {
+        require(block.timestamp >= deadline, 'Still time left!');
+        require(msg.sender == _to, 'You are not the owner!');
+        uint256 tokenBalance = _token.balanceOf(address(this));
+        _token.transfer(_to ,tokenBalance);
+        _to.transfer(1 ether);
         emit Transferred(_to,balance[_to]);
     }
 }
